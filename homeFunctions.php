3eb30542a0213ee294450c31adbb41f5d6137f98
steebi <farrelss@tcd.ie>
2015-04-27 10:40:17 +0100

    Delete function should work now

--- homeFunctions.php
+++ homeFunctions.php
@@ -162,6 +162,7 @@ function deleteLibrary($email, $libID){
         $trashID->bindParam(":email", $email);
         $trashID->execute();
         $trash = $trashID->fetch();
+        print_r($trash);
         
         //use trash id to move all the references with th library ID to b deleted to trash
         $moveOldReferences = $connection->prepare("UPDATE reference SET libID = :trash WHERE libID = :deleteLib");
