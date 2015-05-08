99f9de562d15ca1aa296f164ca5a80da9e3b40e8
steebi <farrelss@tcd.ie>
2015-05-08 10:36:51 +0100

    Added PHP for the phone gap assignment!

--- php/check.php
+++ php/check.php
@@ -1,7 +1,7 @@
 <?php
 
    if ($_POST['formData']){
-    include 'services/config.php';
+    //include 'services/config.php';
      // Prevent caching.
      header('Cache-Control: no-cache, must-revalidate');
      header('Expires: Mon, 01 Jan 1996 00:00:00 GMT');
@@ -13,8 +13,9 @@
      $username = $formarray['username'];;
      $password = $formarray['password'];;
    
-      $con = pg_connect("host=$dbhost dbname=$dbname user=$dbuser password=$dbpass port=5432") or die ("Could not connect: " . pg_last_error());
-   
+      //$con = pg_connect("host=$dbhost dbname=$dbname user=$dbuser password=$dbpass port=5432") or die ("Could not connect: " . pg_last_error());
+         $conn = new PDO('mysql:host=isedbserver.cloudapp.net;port=3306;dbname=user5', "user5", "poi456!!");
+
       $query = ("INSERT INTO login VALUES('$username','$password')");
       $result = pg_query($query);
    
@@ -22,5 +23,7 @@
      // Lets say everything is in order
      $output = array('status' => true, 'message' => 'Welcome!','user'=>$username,'pass'=>$password);
      echo json_encode($output);
+   }    else{
+       echo "No form data";
    }
    ?>
